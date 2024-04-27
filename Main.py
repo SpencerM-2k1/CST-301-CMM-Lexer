da60fe3a8afa99f2302302f8cebd3e4e73f52ee1
Spencer Meren <spencer.meren@gmail.com>
2024-04-26 20:47:06 -0700

    Added C-- example files & file loading

--- Main.py
+++ Main.py
@@ -1,23 +1,59 @@
 from antlr4 import *
 from grammar.CMMLexer import CMMLexer
 
-#Test strings: uncomment code here
+import sys
 
-# inputString = "int x = 5;"
-# inputString = "if (x < 4) write 5;"
-inputString = "if (x >= [4 - -5]) if (y && z) break;"
+# === EXECUTION START ===
+# Launch arguments
+if(len(sys.argv) > 1): #was a file name specified?
+    #Get File text
+    filePath = sys.argv[1]
+    f = open(filePath, "r")
+    inputString = f.read()
+    f.close()
 
-# Set up Lexer
-inputStream = InputStream(inputString)
-lexer = CMMLexer(inputStream)
+    # Set up Lexer
+    inputStream = InputStream(inputString)
+    lexer = CMMLexer(inputStream)
 
-# Get the symbolic names list
-#   (Allows for a more readable output)
-symbolicNames = lexer.symbolicNames
+    # Get the symbolic names list
+    #   (Allows for a more readable output)
+    symbolicNames = lexer.symbolicNames
 
-# Iterate through all tokens
-token = lexer.nextToken()
-while token.type != Token.EOF:
-    tokenName = symbolicNames[token.type]
-    print("Token Type: {}, \tToken Value: {}".format(tokenName, token.text))
+    # Iterate through all tokens
     token = lexer.nextToken()
+    while token.type != Token.EOF:
+        tokenName = symbolicNames[token.type]
+        print("Token Type: {}, \tToken Value: {}".format(tokenName, token.text))
+        token = lexer.nextToken()
+    print()
+else:
+    while (True):
+        print("Enter C-- code for the lexer to tokenize:")
+        inputString = input()
+        
+        if (inputString == ""):
+            break
+
+        # Set up Lexer
+        inputStream = InputStream(inputString)
+        lexer = CMMLexer(inputStream)
+
+        # Get the symbolic names list
+        #   (Allows for a more readable output)
+        symbolicNames = lexer.symbolicNames
+
+        # Iterate through all tokens
+        token = lexer.nextToken()
+        while token.type != Token.EOF:
+            tokenName = symbolicNames[token.type]
+            print("Token Type: {}, \tToken Value: {}".format(tokenName, token.text))
+            token = lexer.nextToken()
+        print()
+
+    # #Helper function to load a text file
+    # def loadText(filePath):
+    #         f = open(filePath, "r")
+    #         fileString = f.read()
+    #         f.close()
+    #         return fileString
